154a73a0b9a48531cba9e403965cee0903894bd7
sinanazem <sinanaazem@gmail.com>
2024-04-05 16:44:06 +0330

    update

--- config/home/urls.py
+++ config/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, ProductDetailView
+from .views import HomeView, ProductDetailView, BucketHomeView
 
 app_name = "home"
 urlpatterns = [
     path('', HomeView.as_view(), name="home" ),
+    path('bucket/', BucketHomeView.as_view(), name="bucket" ),
     path('<slug:slug>/', ProductDetailView.as_view(), name="product_detail" ),
 ]
--- config/home/views.py
+++ config/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Product
+from .tasks import all_bucket_objects_task
 
 
 class HomeView(View):
@@ -14,3 +15,10 @@ class ProductDetailView(View):
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
         return render(request, "home/product_detail.html", {"product":product})
+
+class BucketHomeView(View):
+    template_name = "home/bucket.html"
+    
+    def get(self, request):
+        objects = all_bucket_objects_task()
+        return render(request, self.template_name, {"objects":objects})
